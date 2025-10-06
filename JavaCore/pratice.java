49b95c3e5167e6fb0dc348c373b6ac63ad020593
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-07 00:56:01 +0530

    Auto update: 2025-10-07 00:56:01

--- JavaCore/pratice.java
+++ JavaCore/pratice.java
@@ -3,6 +3,9 @@ package JavaCore;
 public class pratice {
 
     public static void main(String[] args){
-        System.out.println("hello World");
+
+        int a = 12;
+        int b = 13;
+        System.out.println("The Two Number of add: "+ (a + b));
     }
 }
