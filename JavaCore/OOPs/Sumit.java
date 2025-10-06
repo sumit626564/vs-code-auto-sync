89bc8a1a7a38d1dc0a47b234b68b3e37e9d89485
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-07 01:10:01 +0530

    Auto update: 2025-10-07 01:10:01

--- JavaCore/OOPs/Sumit.java
+++ JavaCore/OOPs/Sumit.java
@@ -1,5 +1,14 @@
 package JavaCore.OOPs;
 
+
+class Car{
+    void Vechicle(){
+        System.out.println("This is a Vechicle `Xuv100` ");
+    }
+}
 public class Sumit {
     
+    public static void main(String[] args){
+        
+    }
 }
