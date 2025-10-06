94df697d4b25d5fe3eb2952c7be4ba3c6795a79d
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-07 01:14:01 +0530

    Auto update: 2025-10-07 01:14:01

--- JavaCore/OOPs/Sumit.java
+++ JavaCore/OOPs/Sumit.java
@@ -6,9 +6,16 @@ class Car{
         System.out.println("This is a Vechicle `Xuv100` ");
     }
 }
+class Bike extends Car{
+    void Display(){
+        System.out.println("This is Bike Model Number: `U13231083K3`");
+    }
+}
 public class Sumit {
     
     public static void main(String[] args){
-        
+        Bike bk = new Bike();
+        bk.Vechicle();  // Print Vechicle Model name
+        bk.Display();  // Print Bike Model Number
     }
 }
