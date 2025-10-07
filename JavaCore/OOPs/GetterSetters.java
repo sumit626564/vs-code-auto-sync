a214f9f2d362e328631bb5388666fc7a63a6d896
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 01:06:01 +0530

    Auto update: 2025-10-08 01:06:01

--- JavaCore/OOPs/GetterSetters.java
+++ JavaCore/OOPs/GetterSetters.java
@@ -6,9 +6,17 @@ package JavaCore.OOPs;
  * 
  * Note:- "this": this keyword is used to refer to the current object
  */
+
 public class GetterSetters {
     
     public static void main(String[] args) {
         
     }
+}
+
+class Pen{
+    String color;
+    int tip;
+
+    int getColor()
 }
