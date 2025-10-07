866af4a01f27a678a1b8467687797eb5c16e8300
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-07 20:20:01 +0530

    Auto update: 2025-10-07 20:20:01

--- WebDev/OOPs/J1.js
+++ WebDev/OOPs/J1.js
@@ -1,1 +1,23 @@
 // ! Object : used to store the data in the form of key value pair
+// ! 3
+// ? 1.Object literal
+//var/let/const object_name = {
+    // key1 : value1,
+    // key2 : value2,
+    // key3 : value3,
+    // ..........
+    // .........
+//}
+
+const obj = {
+    unmae : 'Pavi',
+    uid : 1234,
+    address : 'bng'
+}
+console.log(obj);
+
+// dot notation and bracket notation
+// ! access -- objectname.keyname  / objectname["keyname"]
+
+console.log(obj.unmae);
+console.log(obj["uid"]);
