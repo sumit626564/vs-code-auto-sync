96c042595bcb978cdb44c93947dbed832ff9f8fa
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 01:40:01 +0530

    Auto update: 2025-10-08 01:40:01

--- JavaCore/OOPs/GetterSetter.java
+++ JavaCore/OOPs/GetterSetter.java
@@ -7,36 +7,30 @@ package JavaCore.OOPs;
  * Note:- "this": this keyword is used to refer to the current object
  */
 
-public class GetterSetter{
-    
-    public static void main(String[] args) {
-        Pen pen = new Pen();
-        pen.setColor("Blue");
-        System.out.println(pen.getColor());
-        pen.setTip(5);
-        System.out.println(pen.getTip());
-        pen.setColor("Yellow");
-        System.out.println(pen.getColor());
-    }
-}
-
-class Pen{
+class Pencil{
     private String color;
     private int tip;
 
     // Getter function
     String getColor(){
-        return this.color;  // this is special keywords in java, which is point to particular propertip
+        return this.color;  // this is special keywords in java, which is point to particular properties
     }
     int getTip(){
         return this.tip;
     }
-
-    // Setter function
-    void setColor(String newColor){
-        color = newColor;
+    void setColor(String color_new){
+        color = color_new;
     }
     void setTip(int newTip){
         tip = newTip;
+    }
+}
+public class GetterSetter{
+    
+    public static void main(String[] args) {
+        Pencil pen = new Pencil();  // call by Object
+        pen.setColor("Brown");
+        System.out.println(pen.getColor()); // return blue
+        pen.setTip(0);
     }
 }
