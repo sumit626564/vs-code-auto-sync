4320f2e71d02d5ce552e1393ea4881e34af15ab0
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 01:44:01 +0530

    Auto update: 2025-10-08 01:44:01

---
+++ JavaCore/OOPs/Encapsulation.java
@@ -0,0 +1,6 @@
+package JavaCore.OOPs;
+
+/* Encapsulation is defined as the "wrapping up" of data & methods under a single unit. It also implements data hiding.*/
+public class Encapsulation {
+    
+}
