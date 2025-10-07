c2b9e6497e5ec3d1b9208d77904cde0f41771535
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 00:36:01 +0530

    Auto update: 2025-10-08 00:36:01

--- JavaCore/OOPs/ClassObject.java
+++ JavaCore/OOPs/ClassObject.java
@@ -1,4 +1,24 @@
 package JavaCore.OOPs;
 
+public class ClassObject {
 
+    public static void main(String[] args){
 
+    }
+}
+
+class Pen{
+    // Properties  & Function
+    // Below the String and int is properties
+    /* String color
+     * int tip
+     */
+
+    String color;
+    int tip;
+
+    // creating function
+    void setColor(String colo)
+}
+
+class Animal
