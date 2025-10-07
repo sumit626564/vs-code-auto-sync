a30c6249a88fc20f662a646f1e8366a83a7aa309
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 00:56:01 +0530

    Auto update: 2025-10-08 00:56:01

--- JavaCore/OOPs/AccessModifers.java
+++ JavaCore/OOPs/AccessModifers.java
@@ -6,10 +6,16 @@ public class AccessModifers {
         BankAccount ba = new BankAccount();
         ba.username = "Sumit Singh";
         System.out.println("The uername is: " + ba.username);
+        // if you  try to access the pass word , so then you can't able to access password
+
     }
 }
 
 class BankAccount {
     public String  username;
     private String password;
+    public void setPassword(String pwd){
+        password = pwd;
+    }
+
 }
