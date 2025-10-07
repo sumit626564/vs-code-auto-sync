9c331c8a8ed47d957186c192497a311cf7129f4c
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 01:32:01 +0530

    Auto update: 2025-10-08 01:32:01

--- JavaCore/OOPs/GetterSetter.java
+++ JavaCore/OOPs/GetterSetter.java
@@ -1,5 +1,42 @@
 package JavaCore.OOPs;
 
-public class GetterSetter {
+/*Getters & Setters
+ *      Get: to return the value
+ *      Set: to modfiy the value
+ * 
+ * Note:- "this": this keyword is used to refer to the current object
+ */
+
+public class GetterSetter{
     
+    public static void main(String[] args) {
+        Pen pen = new Pen();
+        pen.setColor("Blue");
+        System.out.println(pen.getColor());
+        pen.setTip(5);
+        System.out.println(pen.getTip());
+        pen.setColor("Yellow");
+        System.out.println(pen.getColor());
+    }
+}
+
+class Pen{
+    private String color;
+    private int tip;
+
+    // Getter function
+    String getColor(){
+        return this.color;  // this is special keywords in java, which is point to particular propertip
+    }
+    int getTip(){
+        return this.tip;
+    }
+
+    // Setter function
+    void setColor(String newColor){
+        color = newColor;
+    }
+    void setTip(int newTip){
+        tip = newTip;
+    }
 }
