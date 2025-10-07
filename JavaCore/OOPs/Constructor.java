d8d503b1e532aaaaa2b6950200b7c0aeff6a4184
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 01:56:01 +0530

    Auto update: 2025-10-08 01:56:01

--- JavaCore/OOPs/Constructor.java
+++ JavaCore/OOPs/Constructor.java
@@ -7,11 +7,15 @@ class Tractor{
     Tractor(String name){
         this.model = name;
     }
+    Tractor(int Roll){
+        this.roll = Roll;
+    }
 }
 public class Constructor {
     
     public static void main(String[] args) {
         Tractor tr = new Tractor("Eicher 380");
         System.out.println(tr.model);
+        Tractor tr = new Tractor("Eicher" "380" );
     }
 }
