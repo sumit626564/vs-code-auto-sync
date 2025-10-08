6c60531d3ea322833fec38443b2af91d6e545c39
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-08 07:42:01 +0530

    Auto update: 2025-10-08 07:42:01

---
+++ JavaCore/Array/FunArr.java
@@ -0,0 +1,11 @@
+package JavaCore.Array;
+
+public class FunArr {
+    public static void main(String[] args) {
+        int arr[] = {2, 3, 4, 5, 6};
+        acceptArray(arr);
+    }
+    public static void  acceptArray(int[] a){
+        
+    }
+}
