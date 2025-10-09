2603c1781e79247f0dddb86fab2c9f019575da01
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-09 19:54:01 +0530

    Auto update: 2025-10-09 19:54:01

--- WebDev/JavaScript/iteratorArray.js
+++ WebDev/JavaScript/iteratorArray.js
@@ -24,3 +24,21 @@ for(let k in bike){
     // console.log(k)
     console.log(bike[k]);
 }
+
+// ! Date object
+let d = new Date()
+console.log(d);
+
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.Date());
+console.log(d.getDay());
+console.log(d.getTime());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+
+// "! 1947 aug 15" if you want to paricualr date according to user or itself
+d.setFullYear(1947)
+d.setMonth(0)
+d.set
