0c4e97c8c0ddf3ebf74acf2f7ca2c1a904cc3d9b
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-09 18:06:01 +0530

    Auto update: 2025-10-09 18:06:01

--- JavaCore/OOPs/AbstractMehtod.java
+++ JavaCore/OOPs/AbstractMehtod.java
@@ -11,4 +11,16 @@ abstract class animal{
     void eat() {
         System.out.println("animal eats");
     }
+
+    abstract void walk();  // we no need to implemnt current class we will implemnt in next class
+}
+
+class Horse extends animal{
+    void walk(){
+        System.out.println("Walks on 4 legs");
+    }
+}
+
+class Chicken extends Animla {
+
 }
