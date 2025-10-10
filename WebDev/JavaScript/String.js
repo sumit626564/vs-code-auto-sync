25e863e5a595e906865a809ec4635df66c5444c8
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-10 19:48:01 +0530

    Auto update: 2025-10-10 19:48:01

---
+++ WebDev/JavaScript/String.js
@@ -0,0 +1,9 @@
+// ! String : collection of characters
+let str = 'Hello Good Evening'
+console.log(str);
+console.log(str.length);
+console.log(str[2]);
+
+// ! String methods
+// ! Slice(start_index, end_index) : to get the part of the string
+console.log()
