09e9bff1f150f57b6020aa431ee681226fda1b2e
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-10 20:58:01 +0530

    Auto update: 2025-10-10 20:58:01

---
+++ WebDev/JavaScript/BOM/1Bom.js
@@ -0,0 +1,7 @@
+console.log(window);
+console.log(window.location);
+console.log(location);
+console.log(window.navigator);
+console.log(screen);
+console.log(history);
+console.log(document);  //DOM
