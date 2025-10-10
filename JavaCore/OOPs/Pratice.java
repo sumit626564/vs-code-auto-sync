7a4668294776934e0b17a1d8b68e611261fab399
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-10 23:10:01 +0530

    Auto update: 2025-10-10 23:10:01

--- JavaCore/OOPs/Pratice.java
+++ JavaCore/OOPs/Pratice.java
@@ -1,5 +1,14 @@
 package JavaCore.OOPs;
 
+// find out the correct statment to assign name to object
+class Student{
+    String name;
+    int marks;
+}
 public class Pratice {
-    
+    public static void main(String[] args){
+        Student stu = new Student();
+        stu.name = "sumit";
+        System.out.println(stu.name);
+    }
 }
