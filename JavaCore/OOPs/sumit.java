a57abf64c5d82eda81f2e4d8fe5c38f2bacbeec1
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-11 13:46:01 +0530

    Auto update: 2025-10-11 13:46:01

--- JavaCore/OOPs/sumit.java
+++ JavaCore/OOPs/sumit.java
@@ -2,25 +2,13 @@ package JavaCore.OOPs;
 
 
 class laptop{
-    String name;
-    int year_old;
-    String processor;
-
-    void Datacess(String nme, int old, String proc){
-        name = nme;
-        year_old = old;
-        processor = proc;
+    void print(){
+        System.out.println();
     }
 }
 public class sumit {
 
     public static void main(String[] args) {
-        laptop lp = new laptop();
-        lp.name = "HP";
-        System.out.println("Your Laptop Brand Name: "+ lp.name);
-        lp.processor = "Ryzen 5 5500U";
-        System.out.println("Processor: "+ lp.processor);
-        lp.year_old = 2;
-        System.out.println("Year Old: "+ lp.year_old);
+        
     }
 }
