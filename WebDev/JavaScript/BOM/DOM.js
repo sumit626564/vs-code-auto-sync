034d7f667f713248a705ddcdfbd3f4d497b4efd6
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-11 20:42:01 +0530

    Auto update: 2025-10-11 20:42:01

--- WebDev/JavaScript/BOM/DOM.js
+++ WebDev/JavaScript/BOM/DOM.js
@@ -1,1 +1,8 @@
 let heading = document.getElementById('greet');
+console.log(heading);
+
+//! ! innerHTML : change the HTML of an element
+heading.innerHTML = '<i>Good Evening....</i>'
+
+// ! innerText: Change the content
+heading.innerText = '<i> Good'
