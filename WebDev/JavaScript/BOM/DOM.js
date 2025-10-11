99c56bb0ce3f100a63332ad1525f93c71ccaa906
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-11 20:44:01 +0530

    Auto update: 2025-10-11 20:44:01

--- WebDev/JavaScript/BOM/DOM.js
+++ WebDev/JavaScript/BOM/DOM.js
@@ -5,4 +5,11 @@ console.log(heading);
 heading.innerHTML = '<i>Good Evening....</i>'
 
 // ! innerText: Change the content
-heading.innerText = '<i> Good'
+heading.innerText = '<i> Good <i>'
+
+// ! changing the value of an attribute
+heading.id = 'demo'
+
+// ! setAttribute('arr_name', "att_val") : adding new attribute
+heading.setAttribute('title', to)
+
