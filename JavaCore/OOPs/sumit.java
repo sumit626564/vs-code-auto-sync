4bf6d1bb3019d58de22cb935e47a7f6a1eec5634
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-11 08:42:01 +0530

    Auto update: 2025-10-11 08:42:01

--- JavaCore/OOPs/sumit.java
+++ JavaCore/OOPs/sumit.java
@@ -1,19 +1,9 @@
 package JavaCore.OOPs;
 
+
 public class sumit {
+    
     public static void main(String[] args) {
-        mother fh = new mother();
-        fh.namesu = "Bhaskar Singh";
-        System.out.println("Father Name: "+fh.namesu);
-        fh.age = 54;
-        System.out.println("Father Age: "+ fh.age);
+        
     }
-}
-class Singh{
-    String name;
-    int weight;
-}
-class mother extends Singh{
-    int age;
-    String namesu;
 }
