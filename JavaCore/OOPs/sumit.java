736a0dbabc8e39020214136065e8408273d4f83d
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-11 14:48:01 +0530

    Auto update: 2025-10-11 14:48:01

--- JavaCore/OOPs/sumit.java
+++ JavaCore/OOPs/sumit.java
@@ -15,7 +15,7 @@ public class sumit {
 
     public static void main(String[] args) {
         laptop obj1 = new hp();
-        obj1
+        obj1.print();
         laptop obj2 = new laptop();
         obj2.print();
     }
