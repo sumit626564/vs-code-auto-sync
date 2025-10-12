c0859fcae83c46202328698e7f78eed35d629a37
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 16:02:01 +0530

    Auto update: 2025-10-12 16:02:01

--- JavaCore/FileHandeling/FileRead.java
+++ JavaCore/FileHandeling/FileRead.java
@@ -1,10 +1,20 @@
 package JavaCore.FileHandeling;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.util.Scanner;
-public class FileRead {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class FileRead {
     public static void main(String[] args) {
-        try
+        try{
+            FileWriter newfile = new FileWriter("stuu.txt");
+            newfile.write("This is name");
+            newfile.write("Name: Sumit Singh");
+            newfile.write("BTech");
+
+            newfile.close();   /// compulsory
+            System.out.println("File has been writtend into.");
+        }catch(IOException e){
+            System.out.println("Error.....!");
+            e.printStackTrace();
+        }
     }
 }
