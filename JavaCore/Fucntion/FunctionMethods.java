8842775d5a7ef5125dc8fc9e3d0bb2f4798ae060
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 13:18:01 +0530

    Auto update: 2025-10-12 13:18:01

--- JavaCore/Fucntion/FunctionMethods.java
+++ JavaCore/Fucntion/FunctionMethods.java
@@ -1,6 +1,16 @@
 package JavaCore.Fucntion;
 
-/* How to write and call fucntions(methods) */
-public class FunctionManyWay {
-    
+/* How to write and call fucntions(methods) inside a class */
+
+class Student{
+    void Study(){           //  Function (method)
+        System.out.println("The Student is studying...!");
+    }
+}
+
+public class FunctionMethods {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.Study();
+    }    
 }
