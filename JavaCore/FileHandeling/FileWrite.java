3704f7a52731a2e3f203b03b614c462487cb2cbe
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 18:58:02 +0530

    Auto update: 2025-10-12 18:58:02

--- JavaCore/FileHandeling/FileWrite.java
+++ JavaCore/FileHandeling/FileWrite.java
@@ -6,7 +6,7 @@ public class FileWrite {
     public static void main(String[] args) {
         try{
             FileWriter newdata = new FileWriter("stuu.txt");
-            String datainsert = "Artifical Intelligence(AI) is the simulation of human intelligence"
+            String datainsert = "Artifical Intelligence(AI) is the simulation of human intelligence"+"in machines that are programmed to think humans and mimic their actions.";
             newdata.write("null");
 
         }
