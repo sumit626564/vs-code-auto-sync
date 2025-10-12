887889a9067ea79d24d6a23231bf299770f36c37
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 15:38:01 +0530

    Auto update: 2025-10-12 15:38:01

--- JavaCore/FileHandeling/Create.java
+++ JavaCore/FileHandeling/Create.java
@@ -2,11 +2,13 @@ package JavaCore.FileHandeling;
 import java.io.File;
 import java.io.IOException;
 
-public class CreateFile {
+public class Create {
     public static void main(String[] args){
         try{
             File newfile = new File('stu.txt');
-            if(newfile.createNewFile())
+            if(newfile.createNewFile()){
+                System.out.println("Task completed: "+ newfile.getName());
+            }
         }
     }
 }
