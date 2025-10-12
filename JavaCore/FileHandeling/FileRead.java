4e4dcfdb417a1ce7e5b5fc337819fba5a4ca663e
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 15:54:01 +0530

    Auto update: 2025-10-12 15:54:01

--- JavaCore/FileHandeling/FileRead.java
+++ JavaCore/FileHandeling/FileRead.java
@@ -1,1 +1,10 @@
-d
+package JavaCore.FileHandeling;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.util.Scanner;
+public class FileRead {
+
+    public static void main(String[] args) {
+        try
+    }
+}
