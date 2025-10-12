437c73778fc42e96e9ff23f674cc2c6a40463d1e
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 15:40:01 +0530

    Auto update: 2025-10-12 15:40:01

--- JavaCore/FileHandeling/Create.java
+++ JavaCore/FileHandeling/Create.java
@@ -5,10 +5,15 @@ import java.io.IOException;
 public class Create {
     public static void main(String[] args){
         try{
-            File newfile = new File('stu.txt');
+            File newfile = new File(stu.txt);
             if(newfile.createNewFile()){
                 System.out.println("Task completed: "+ newfile.getName());
+            }else{
+                System.out.println("Sorry Not Completed....!");
             }
+        }catch (IOException e){
+            System.out.println("Error....!");
+            e.printStackTrace();
         }
     }
 }
