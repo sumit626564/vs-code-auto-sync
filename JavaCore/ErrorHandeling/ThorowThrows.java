73b8fbc6561f1e00be115f8059ae6d0de8879301
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 12:04:01 +0530

    Auto update: 2025-10-12 12:04:01

--- JavaCore/ErrorHandeling/ThorowThrows.java
+++ JavaCore/ErrorHandeling/ThorowThrows.java
@@ -1,5 +1,16 @@
 package JavaCore.ErrorHandeling;
 
 public class ThorowThrows {
-    
+    public static void main(String[] args){
+
+    }
+}
+class ThrowExample{
+    void divide(int a, int b){
+        if(a==b){
+            throw new ArithmeticException("You are not elegible to vote!");
+        }else{
+            System.out.println("You can");
+        }
+    }
 }
