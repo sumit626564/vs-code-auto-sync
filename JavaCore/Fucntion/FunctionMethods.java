2b105a909bea90d16622068fdc9024c1d8dd0b57
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-12 13:20:01 +0530

    Auto update: 2025-10-12 13:20:01

--- JavaCore/Fucntion/FunctionMethods.java
+++ JavaCore/Fucntion/FunctionMethods.java
@@ -2,12 +2,19 @@ package JavaCore.Fucntion;
 
 /* How to write and call fucntions(methods) inside a class */
 
+/* Basic -- Function inside class, call using object */
 class Student{
     void Study(){           //  Function (method)
         System.out.println("The Student is studying...!");
     }
 }
 
+/* Function with parameters and return type */
+class Calculator{
+    int add(int a, int b){
+        return a+b;
+    }
+}
 public class FunctionMethods {
     public static void main(String[] args) {
         Student s1 = new Student();
