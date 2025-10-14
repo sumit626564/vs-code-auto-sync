6e60d8388b43b4dcf6aad569427857b8fc11d859
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-14 09:40:01 +0530

    Auto update: 2025-10-14 09:40:01

--- JavaCore/OOPs/ConstructorChaining.java
+++ JavaCore/OOPs/ConstructorChaining.java
@@ -19,10 +19,29 @@ class Student{
     // Constructor 1
     Student(){
         this("Unknown", 18); // calling constructor 2
+        System.out.println("Default Constructor Callled");
+    }
+
+    // Constructor 2
+    Student(String name, int age){
+        this(name, age, "Not Assigned");  // calling constructor 3
+        System.out.println("Two-Parameter Costructor Called");
+    }
+
+    // Constructor 3
+    Student(String name, int age, String course){
+        this.name = name;
+        this.age = age;
+        this.course = course;
+        System.out.println("Three-Parameter Constructor Called");
+    }
+    void display(){
+        System.out.println("Name: "+ name + ", Age: "+ age + ", Course: "+ course);
     }
 }
 public class ConstructorChaining {
     public static void main(String[] args) {
-        
+        Student s1 = new Student();
+        s1.display();
     }
 }
