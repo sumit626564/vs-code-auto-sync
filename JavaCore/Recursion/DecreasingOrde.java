6a5becc19742e5f74409368ea84cb508b74e6a18
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-14 15:18:01 +0530

    Auto update: 2025-10-14 15:18:01

--- JavaCore/Recursion/DecreasingOrde.java
+++ JavaCore/Recursion/DecreasingOrde.java
@@ -9,13 +9,22 @@ class IncreasingDecreasing{
         System.out.print(downward + ", ");
         Decreasing(downward - 1);   // recursive call 
     }
+
+    public void Increasing(int upward){
+        // Base Condition: stop when upward reaches 0(zero)
+        if( upward > 10){
+            return; // stop
+        }
+        System.out.println(upward + ", ");
+        Increasing(upward + 1);
+    }
 }
 public class DecreasingOrde {
     
     public static void main(String[] args) {
         IncreasingDecreasing  NewIncreasingOrder = new IncreasingDecreasing();
         NewIncreasingOrder.Decreasing(10);
-        
+        NewIncreasingOrder.Increasing(10);
         System.out.println();  // for a clean new line after output
     }
 }
