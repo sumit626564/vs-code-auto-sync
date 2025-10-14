4472ee336682a168ea79c319b9df1f6e5b3d4798
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-14 14:30:01 +0530

    Auto update: 2025-10-14 14:30:01

--- JavaCore/Recursion/RecOne.java
+++ JavaCore/Recursion/RecOne.java
@@ -1,7 +1,10 @@
 package JavaCore.Recursion;
 
 public class RecOne {
-    
+    public static void printDec(int n){
+        System.out.print(n);
+        
+    }
     public static void main(String[] args) {
         int n = 10;
     }
