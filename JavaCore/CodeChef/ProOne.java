68ca17bb7ef1bb62f6ac0c7d801435527245a564
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-16 23:40:01 +0530

    Auto update: 2025-10-16 23:40:01

---
+++ JavaCore/CodeChef/ProOne.java
@@ -0,0 +1,12 @@
+package JavaCore.CodeChef;
+
+class ScoreConsistent{
+    public int currentScore(int a, int b){
+        
+    }
+}
+public class ProOne {
+    public static void main(String[] args) {
+        
+    }
+}
