593b1c34c7cb8534fc60502d274d91e10fffa6de
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-17 05:00:01 +0530

    Auto update: 2025-10-17 05:00:01

--- BashScript/operator.sh
+++ BashScript/operator.sh
@@ -8,6 +8,14 @@ b=3
 echo $((a+b))
 echo "Product: $((a * b))"
 echo "Division: $((a / b))"
-################################
 
+#####################################
+# Relational Operator(for NUmbers)
+#####################################
 
+# -eq  (equal to)
+# -ne  (not equal to)
+# -gt   (Greater Than )
+# -lt   (less than)
+# -ge   (Greater than)
+# -le   (less than or equal)
