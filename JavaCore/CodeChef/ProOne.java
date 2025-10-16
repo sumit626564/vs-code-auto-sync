46150d868c52ff4454ab15c912d42ff6753a4082
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-17 00:05:01 +0530

    Auto update: 2025-10-17 00:05:01

--- JavaCore/CodeChef/ProOne.java
+++ JavaCore/CodeChef/ProOne.java
@@ -2,14 +2,37 @@ package JavaCore.CodeChef;
 
 import java.util.Scanner;
 
+class FootBallMatch{
+    int currA, currB, laterC, laterD;
 
+    // constructor
+    FootBallMatch(int currA, int currB, int laterC, int laterD){
+        this.currA = currA; 
+        this.currB = currB;
+        this.laterC = laterC;
+        this.laterD = laterD;
+    }
+
+    // Function to check if future Score is Possible
+    public String CheckPossible(){
+        if(laterC >= currA && laterD >= currB){
+            return "POSSIBLE";
+        }else{
+            return "IMPOSSIBLE";
+        }
+    }
+}
 public class ProOne {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int testcase = input.nextInt();
-        while(testcase < 1){
-            int
-        }
+        int currA = input.nextInt();
+        int currB = input.nextInt();
+        int laterC = input.nextInt();
+        int laterD = input.nextInt();
+
+        // Create Object and call fucntion
+        FootBallMatch match = new FootBallMatch(currA, currB, laterC, laterD);
+        System.out.println(match.CheckPossible());
         input.close();
     }
 }
