a472c679e7d67f46fda022cb97e3d6c797a29af0
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-16 20:20:02 +0530

    Auto update: 2025-10-16 20:20:02

--- JavaCore/Array/ArrayInput.java
+++ JavaCore/Array/ArrayInput.java
@@ -2,7 +2,7 @@ package JavaCore.Array;
 
 import java.util.Scanner;
 
-public class HackerArray {
+public class ArrayInput {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -10,11 +10,8 @@ public class HackerArray {
 
         // creating array
         int[] simple = new int[narray];
-        while (narray > 1) {
-            simple[narray] = input.nextInt();
-        }
-        System.out.println("Phy: "+ simple);
-        
+        for(int i)
+    
 
         input.close();
 
