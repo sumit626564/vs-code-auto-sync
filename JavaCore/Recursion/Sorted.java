d802fd15236cbd0ad00e4d29e1c69172a240ae30
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-16 09:25:01 +0530

    Auto update: 2025-10-16 09:25:01

--- JavaCore/Recursion/Sorted.java
+++ JavaCore/Recursion/Sorted.java
@@ -2,6 +2,14 @@ package JavaCore.Recursion;
 
 public class Sorted {
     public static void main(String[] args){
-        int sum = 12, add = 12;
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        
+        if(N % 2 == 0 && (N > 2 && N < 5)){
+            System.out.println("Not Weird");
+        }else if(N % 2 == 0 && (N >6 && N < 20)){
+            System.out.println("Weird");
+        }else{
+            System.out.println("Weird");
+        }
     }
 }
