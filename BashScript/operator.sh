247f170a227e40473b7d9066df1ff497d18cc516
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-17 04:50:01 +0530

    Auto update: 2025-10-17 04:50:01

--- BashScript/operator.sh
+++ BashScript/operator.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
+:'arthimetic Operations
+$(( expression ))
+# comprasion:- -eq, -ne, -lt, -le, -gt, -ge
+# String:- =, !=, -z, -n
+'
 a=5
 b=3
 echo $((a+b))
 
-# comprasion:- -eq, -ne, -lt, -le, -gt, -ge
-# String:- =, !=, -z, -n
