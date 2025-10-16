887e8f1ad212a765bc079de17fe44098fa09349f
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-16 18:25:01 +0530

    Auto update: 2025-10-16 18:25:01

--- JavaCore/DataTypes/PrimitiveData.java
+++ JavaCore/DataTypes/PrimitiveData.java
@@ -1,5 +1,6 @@
 package JavaCore.DataTypes;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 /* Primitive data  */
@@ -9,7 +10,11 @@ public class PrimitiveData {
         Scanner input = new Scanner(System.in);
         int testCase = input.nextInt();
 
-        // Ranges for each data type as Big
+        // Ranges for each data type as BigInteger for Large number
+        BigInteger byteMin = BigInteger.valueOf(Byte.MIN_VALUE);
+        BigInteger byteMax = BigInteger.valueOf(Byte.MAX_VALUE);
+        BigInteger shortMin = BigInteger.valueOf(Byte.MIN_VALUE);
+        BigInteger
         input.close();
     }
 }
