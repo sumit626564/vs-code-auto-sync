31edeea435e91ea45f37462695cc8ff9a3dc6504
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-16 09:30:01 +0530

    Auto update: 2025-10-16 09:30:01

--- JavaCore/Recursion/Sorted.java
+++ JavaCore/Recursion/Sorted.java
@@ -1,15 +1,18 @@
 package JavaCore.Recursion;
 
+import java.util.Scanner;
+
 public class Sorted {
     public static void main(String[] args){
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter Your Number: ");
+        int N = input.nextInt();
         
-        if(N % 2 == 0 && (N > 2 && N < 5)){
+        if(N % 2 == 0 && ((N > 2 && N < 5) && N < 20)){
             System.out.println("Not Weird");
-        }else if(N % 2 == 0 && (N >6 && N < 20)){
-            System.out.println("Weird");
         }else{
             System.out.println("Weird");
         }
+        input.close();
     }
 }
