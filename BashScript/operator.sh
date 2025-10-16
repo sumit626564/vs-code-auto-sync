ce50b2af7d176b6eb16d9ad32d4712bcf7bd453a
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-17 05:05:01 +0530

    Auto update: 2025-10-17 05:05:01

--- BashScript/operator.sh
+++ BashScript/operator.sh
@@ -3,8 +3,8 @@
 #arthimetic Operations
 #$(( expression ))
 #  +, -, *, /, %, **(power)
-a=5
-b=3
+a=3
+b=1
 echo $((a+b))
 echo "Product: $((a * b))"
 echo "Division: $((a / b))"
@@ -12,10 +12,24 @@ echo "Division: $((a / b))"
 #####################################
 # Relational Operator(for NUmbers)
 #####################################
-
+#Symbol:-
+#       [$variable -relationalOpName $variable]
+# 
 # -eq  (equal to)
 # -ne  (not equal to)
 # -gt   (Greater Than )
 # -lt   (less than)
 # -ge   (Greater than)
 # -le   (less than or equal)
+
+a=15
+b=10
+if [ $a -gt $b ]; then
+    echo "$a is greater than $b"
+else
+    echo "$a is not greater than $b"
+fi
+
+############################################
+# String Operator
+####################################
