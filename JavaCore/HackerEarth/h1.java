ad3ee0d6d221b536191122a05fcae4742a8d692d
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-16 20:00:01 +0530

    Auto update: 2025-10-16 20:00:01

--- JavaCore/HackerEarth/h1.java
+++ JavaCore/HackerEarth/h1.java
@@ -1,1 +1,14 @@
-pa
+package JavaCore.HackerEarth;
+
+import java.util.Scanner;
+
+public class h1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int narray = input.nextInt();
+
+        // creating array
+        int[] nstore = new int[narray];
+    }
+}
