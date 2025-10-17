df8cbc6fde04ac5431b73edd23f9935c19b6a148
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-17 05:50:01 +0530

    Auto update: 2025-10-17 05:50:01

--- BashScript/Conditional.sh
+++ BashScript/Conditional.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# For Multiple Condition
+
 mark=85
 if [ $mark -ge 90 ]; then
     echo "$ A Grade"
@@ -7,4 +9,16 @@ elif [ $mark -ge 75 ]; then
     echo "B grade "
 else
     echo "C grade"
+fi
+
+##### String Comprasion
+
+echo "Enter Your Name: "
+read name
+if [ "$name" == "Sumit" ]; then
+    echo "Welcome Sumit!"
+elif [ "$name" == "Raj"]; then
+    echo "Welcome Raj!"
+else
+    echo "Unknown user"
 fi
