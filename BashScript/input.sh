6f4f1b2e3668e309fe0258b07df7d407ffc2d1a0
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-17 06:10:01 +0530

    Auto update: 2025-10-17 06:10:01

--- BashScript/input.sh
+++ BashScript/input.sh
@@ -8,3 +8,15 @@ echo -e "\nWelcome to bash tutorial" $entered_name
 echo "Enter your first name and last name: "
 read firstname lastname
 echo "Hello, $firstname $lastname"
+
+# Input in array
+
+echo "Enter numbers seperated by space: "
+read -a numbers
+
+echo "You entered: ${numbers[@]}"
+echo "Sum of numbers: $((numbers[0] + numbers[1] + numbers[2]))"
+
+# Prompting Without "echo"
+read -p "Enter your city and Country: "  city country
+echo "You live in $city $country"
