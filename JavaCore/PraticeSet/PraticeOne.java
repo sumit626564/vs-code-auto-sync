e084488249cde17a76bdc25bdfd518c01877f8e2
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-19 01:25:01 +0530

    Auto update: 2025-10-19 01:25:01

---
+++ JavaCore/PraticeSet/PraticeOne.java
@@ -0,0 +1,10 @@
+package JavaCore.PraticeSet;
+
+public class PraticeOne {
+    public String Declmination(int topicA, int topicB, int topicC, int topicX){
+
+    }
+    public static void main(String[] args){
+
+    }
+}
