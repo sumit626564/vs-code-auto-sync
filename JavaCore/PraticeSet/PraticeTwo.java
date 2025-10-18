8ecbe4d9b3b1241ecbe0b22890d8eb506c95cb56
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-19 02:15:01 +0530

    Auto update: 2025-10-19 02:15:01

--- JavaCore/PraticeSet/PraticeTwo.java
+++ JavaCore/PraticeSet/PraticeTwo.java
@@ -6,16 +6,16 @@ class Monopoly{
     private int profitsR;
     private int profitsS;
 
-    public void Profits(int companyP, int companyQ, int companyR, int companyS){
-        this.profitsP = companyP;
-        this.profitsQ = companyQ;
-        this.profitsR = companyR;
-        this.profitsS = companyS;
+    public void Profits(int companyA, int companyB, int companyC, int companyD){
+        this.profitsP = companyA;
+        this.profitsQ = companyB;
+        this.profitsR = companyC;
+        this.profitsS = companyD;
     }
 
     public void ProfitCalculation(){
-        int compnayA = companyQ + companyR + companyS;
-        int companyB = com
+        int compnayA = profitsQ + profitsR + profitsS;
+        int companyB = prof
     }
 }
 
