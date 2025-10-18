ba0e979840a916bf5156b1ec1675d468e9d75039
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-19 01:40:01 +0530

    Auto update: 2025-10-19 01:40:01

--- JavaCore/PraticeSet/PraticeOne.java
+++ JavaCore/PraticeSet/PraticeOne.java
@@ -1,13 +1,23 @@
 package JavaCore.PraticeSet;
 
+import java.text.DecimalFormatSymbols;
 import java.util.Scanner;
 
 public class PraticeOne {
     public String Declmination(int topicA, int topicB, int topicC, int topicX){
-        if(topicA.){}
+        if(topicA == topicX || topicB == topicX || topicC == topicX){
+            return "yes";
+        }else{
+            return "No";
+        }
     }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        int topicA = input.nextInt();
+        int topicB = input.nextInt();
+        int topicC = input.nextInt();
+        int topicX = input.nextInt();
+        System.out.println(PraticeOne.Declmination(topicA, topicB, topicC, topicX));
         input.close();
     }
 }
