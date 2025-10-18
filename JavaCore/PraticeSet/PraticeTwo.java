ffa063b14b70608d04312316324abcc6816231e6
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-19 02:05:01 +0530

    Auto update: 2025-10-19 02:05:01

---
+++ JavaCore/PraticeSet/PraticeTwo.java
@@ -0,0 +1,22 @@
+package JavaCore.PraticeSet;
+
+class Monopoly{
+    private int companyP;
+    private int companyQ;
+    private int companyR;
+    private int companyS;
+
+    public void Profits(int companyP, int companyQ, int companyR, int companyS){
+        this.companyP = companyP;
+        this.companyQ = companyQ;
+        this.companyR = companyR;
+        this.companyS = companyS;
+    }
+    
+}
+
+public class PraticeTwo {
+    public static void main(String[] args){
+
+    }
+}
