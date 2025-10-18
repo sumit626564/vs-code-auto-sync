afde8c697a3dc739a2d8c127b7abb3b0dfa8e945
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-19 01:35:01 +0530

    Auto update: 2025-10-19 01:35:01

--- JavaCore/PraticeSet/PraticeOne.java
+++ JavaCore/PraticeSet/PraticeOne.java
@@ -1,10 +1,13 @@
 package JavaCore.PraticeSet;
 
+import java.util.Scanner;
+
 public class PraticeOne {
     public String Declmination(int topicA, int topicB, int topicC, int topicX){
-        if(topicA.equal(topicX)){}
+        if(topicA.){}
     }
     public static void main(String[] args){
-
+        Scanner input = new Scanner(System.in);
+        input.close();
     }
 }
