4487e74c46ab8c46b7284d4a68fe850e1bc2e63d
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-19 01:30:01 +0530

    Auto update: 2025-10-19 01:30:01

--- JavaCore/PraticeSet/PraticeOne.java
+++ JavaCore/PraticeSet/PraticeOne.java
@@ -2,7 +2,7 @@ package JavaCore.PraticeSet;
 
 public class PraticeOne {
     public String Declmination(int topicA, int topicB, int topicC, int topicX){
-
+        if(topicA.equal(topicX)){}
     }
     public static void main(String[] args){
 
