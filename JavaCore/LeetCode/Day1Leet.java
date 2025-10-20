246c5dccb7ecc070c1e603b156f61f30130436bd
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-20 21:20:01 +0530

    Auto update: 2025-10-20 21:20:01

--- JavaCore/LeetCode/Day1Leet.java
+++ JavaCore/LeetCode/Day1Leet.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 class Solution{
     public int[] twoSum(int[] nums, int target){
         for(int i=0; i<nums.length-1; i++){
-            if(nums[i] + nums[i+1] == target){
-                return new int[]{i, i+1};
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j+1};
+                }
             }
         }
         return new int[]{};   // if don't match with target
