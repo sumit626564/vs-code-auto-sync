b63b8ab8488b0978b3c19a6527ca5d698d7ad342
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-20 21:30:01 +0530

    Auto update: 2025-10-20 21:30:01

--- JavaCore/LeetCode/Day1Leet.java
+++ JavaCore/LeetCode/Day1Leet.java
@@ -1,7 +1,7 @@
 package JavaCore.LeetCode;
 import java.util.Arrays;
 
-class Solution{
+class ProblemOne{
     public int[] twoSum(int[] nums, int target){
         for(int i=0; i<nums.length-1; i++){
             for(int j=i+1; j<nums.length; j++){
