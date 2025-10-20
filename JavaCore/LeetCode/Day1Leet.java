b3995598aa05e711a1d9557e18810520ac950dc0
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-20 17:30:01 +0530

    Auto update: 2025-10-20 17:30:01

--- JavaCore/LeetCode/Day1Leet.java
+++ JavaCore/LeetCode/Day1Leet.java
@@ -3,12 +3,12 @@ package JavaCore.LeetCode;
 
 public class Day1Leet {
 
-    public static void main(String args){
-        int[] arr = {2, 7, 11, 15};
-        int target = 9;
-        for(int i=0; i<arr.length; i++){
+    public static void main(String[] args){
+        int[] arr = {3, 2, 4};
+        int target = 6;
+        for(int i=0; i<arr.length -1; i++){
             if(arr[i] + arr[i+1] == target){
-                System.out.println(arr[i] + "," + arr[i+1]);
+                System.out.println(i + "," + i+1);
             }
         }
     }
