d0b0bf8a8e19df45aa2390b428c8ac693d5e550b
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-20 23:15:01 +0530

    Auto update: 2025-10-20 23:15:01

--- JavaCore/LeetCode/Demo.java
+++ JavaCore/LeetCode/Demo.java
@@ -3,8 +3,15 @@ package JavaCore.LeetCode;
 public class Demo {
     public static void main(String[] args) {
         int[] newarr = {1, 2, 3, 5};
-        for(int i=newarr.length-1; i>=0; i--){
-            System.out.println(newarr[i]);
+        for(int i=0; i<newarr.length-1; i++){
+            for(int j=newarr.length-1; j>=0; j--){
+                int temp = newarr[i];
+                newarr[i] = newarr[j];
+                newarr[j] = temp;
+                i++;
+                j--;
+                System.out.println(temp);
+            }
         }
     }
 }
