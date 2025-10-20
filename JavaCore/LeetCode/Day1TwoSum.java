787d0d4d55c91e836e94c37e087942de868e0ffd
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-20 17:15:01 +0530

    Auto update: 2025-10-20 17:15:01

--- JavaCore/LeetCode/Day1TwoSum.java
+++ JavaCore/LeetCode/Day1TwoSum.java
@@ -1,3 +1,15 @@
 package JavaCore.LeetCode;
 
-/*  */
+
+public class Day1TwoSum {
+
+    public static void main(String args){
+        int[] arr = {2, 7, 11, 15};
+        int target = 9;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] + arr[i+1] == target){
+                
+            }
+        }
+    }
+}
