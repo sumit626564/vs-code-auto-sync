9e3a806ca6827bd2d11f1c804b78ba3ec65b9daa
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-20 17:50:01 +0530

    Auto update: 2025-10-20 17:50:01

--- JavaCore/LeetCode/Day1Leet.java
+++ JavaCore/LeetCode/Day1Leet.java
@@ -1,10 +1,11 @@
 package JavaCore.LeetCode;
+import java.util.Arrays;
 
 class Solution{
     public int[] twoSum(int[] nums, int target){
         for(int i=0; i<nums.length-1; i++){
             if(nums[i] + nums[i+1] == target){
-                return new int[]{1, i+1};
+                return new int[]{i, i+1};
             }
         }
         return new int[]{};   // if don't match with target
@@ -18,7 +19,7 @@ public class Day1Leet {
         int target = 9;
         Solution sl = new Solution();
         int[] result = sl.twoSum(nums, target);
-        System.out.println(result);
+        System.out.println(Arrays.toString(result));
 
     }
 }
