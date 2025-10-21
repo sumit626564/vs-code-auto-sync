779c5dc18d0200d0d3986f60a38037fb79f00d6f
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 00:25:01 +0530

    Auto update: 2025-10-22 00:25:01

--- JavaCore/HashMapOver/UnionUserInput.java
+++ JavaCore/HashMapOver/UnionUserInput.java
@@ -1,10 +1,22 @@
 package JavaCore.HashMapOver;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class UnionUserInput {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         
+        // First Set
+        System.out.println("Enter element of Set 1 separated by sapce: ");
+        int n1 = input.nextInt();
+        HashSet<Integer> set1 = new HashSet<>();
+        System.out.println("Enter elements for set 1:");
+        for(int i = 0; i < n1; i++){
+            set1.add(input.nextInt());
+        }
+
+        // Second Set
+        System.out.println("Enter of elments for  Set 2: ");
     }
 }
