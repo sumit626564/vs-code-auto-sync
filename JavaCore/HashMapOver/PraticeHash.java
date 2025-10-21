f83b9a9280f6299a8bfbc6d461edbe188352ca43
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-21 12:10:01 +0530

    Auto update: 2025-10-21 12:10:01

---
+++ JavaCore/HashMapOver/PraticeHash.java
@@ -0,0 +1,11 @@
+package JavaCore.HashMapOver;
+import java.util.HashSet;
+
+public class PraticeHash {
+
+    public static void main(String[] args){
+        int[] arr = {2, 4, 6, 8, 9, 7, 5};
+        HashSet<Integer> new_arr = arr HashSet<>(arr);
+
+    }
+}
