49b3db60c2f7b87b4f0f077bb4163ab94a9d8b44
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-21 12:15:01 +0530

    Auto update: 2025-10-21 12:15:01

--- JavaCore/HashMapOver/PraticeHash.java
+++ JavaCore/HashMapOver/PraticeHash.java
@@ -5,7 +5,17 @@ public class PraticeHash {
 
     public static void main(String[] args){
         int[] arr = {2, 4, 6, 8, 9, 7, 5};
-        HashSet<Integer> new_arr = arr HashSet<>(arr);
+        HashSet<Integer> new_arr = new HashSet<>();
+
+        for(int num : arr){
+            new_arr.add(num);
+        }
+
+        // Search- Contains;
+        if(!new_arr.contains(3)){
+            System.out.println("This array not Contains 3");
+        }
+        System.out.println("Hash Element: "+  new_arr);
 
     }
 }
