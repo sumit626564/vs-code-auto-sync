700f61d465c92f54f86fdddfb4902bd20c6f610f
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-21 11:40:01 +0530

    Auto update: 2025-10-21 11:40:01

---
+++ JavaCore/HashMapOver/Hashing.java
@@ -0,0 +1,17 @@
+package JavaCore.HashMapOver;
+import java.util.HashSet;
+
+/* HashSet in Java
+ *  Insert/Add - O(1)
+ *  Search/ Contains - O(1)
+ *  Delete/ Remove - O(1)
+ * 
+ *  Syntax:-
+ *          HashSet<DataType> setname = new HashSet<>();
+ */
+
+public class Hashing {
+    public static void main(String[] args){
+        Has
+    }
+}
