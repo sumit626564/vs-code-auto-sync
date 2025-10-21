7258b1cf43e26f23abda904d403222c9e24d0679
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-21 16:50:01 +0530

    Auto update: 2025-10-21 16:50:01

---
+++ JavaCore/HashMapOver/UnionUserInput.java
@@ -0,0 +1,10 @@
+package JavaCore.HashMapOver;
+
+import java.util.Scanner;
+
+public class UnionUserInput {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+    }
+}
