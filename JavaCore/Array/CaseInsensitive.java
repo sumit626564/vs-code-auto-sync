9e0d1a120fbda4b24efa7067ffd4770f6a70edba
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 18:55:01 +0530

    Auto update: 2025-10-22 18:55:01

---
+++ JavaCore/Array/CaseInsensitive.java
@@ -0,0 +1,12 @@
+package JavaCore.Array;
+
+import java.util.ArrayList;
+
+public class CaseInsensitive {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Sumit");
+        names.add("Rahul");
+        names
+    }
+}
