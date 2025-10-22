067221a64665db3fb131dfe19f24d082d6ac44c5
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 15:25:01 +0530

    Auto update: 2025-10-22 15:25:01

--- JavaCore/Hash_Java/InputHashMap.java
+++ JavaCore/Hash_Java/InputHashMap.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class InputHashMap {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("How many times do you want to enter ?");
+        int n = input.nextInt();
+        input.nextLine();
+        for(int i=0; i<n; i++){
+            
+        }
 
-        String
+
     }
 }
