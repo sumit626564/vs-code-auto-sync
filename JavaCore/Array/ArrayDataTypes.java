52a7ade4edf61a808887fb4d2cd8dce245f82803
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 18:35:02 +0530

    Auto update: 2025-10-22 18:35:02

--- JavaCore/Array/ArrayDataTypes.java
+++ JavaCore/Array/ArrayDataTypes.java
@@ -1,15 +1,29 @@
 package JavaCore.Array;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayDataTypes {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
         ArrayList<Object> mixed = new ArrayList<>();
-        mixed.add("Sumit Singh");   // String
+        mixed.add("Sumit_Singh");   // String
         mixed.add(23);              // Integer
         mixed.add(89.5f);           // Float
+        mixed.add("@: rajputsinghshear@gmail.com");
+
+        // Search for a specific element
+        System.out.print("Enter your searchItem: ");
+        String searchItem = input.nextLine();
+        if(mixed.contains(searchItem)){
+            System.out.println(searchItem + " found at index: "+ mixed.indexOf(searchItem));
+        }else{
+            System.out.println("Not Found:.....!");
+        }
 
         // print element
         System.out.println("Your List: "+ mixed);
+
+        input.close();
     }
 }
