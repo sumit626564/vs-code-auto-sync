f5abdcc9978ebf216585cd4ad6da9b04250de041
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 18:05:01 +0530

    Auto update: 2025-10-22 18:05:01

--- JavaCore/Array/arrayList.java
+++ JavaCore/Array/arrayList.java
@@ -1,7 +1,18 @@
 package JavaCore.Array;
 
-public class ArrayList {
+import java.util.ArrayList;
+
+public class arrayList{
     public static void main(String[] args) {
-        ArrayList
+        ArrayList<String> list =  new ArrayList<>();
+
+        // add elements
+        list.add("apple");
+        list.add("Banana");
+        list.add("Mango");
+
+        // Access element
+        System.out.println("First elemnt: "+list.get(0));
+        
     }
 }
