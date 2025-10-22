a7aef3dc8e0e7fbe3bb0d282b838d1734654e73e
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 14:25:01 +0530

    Auto update: 2025-10-22 14:25:01

---
+++ JavaCore/Hash_Java/InputHashMap.java
@@ -0,0 +1,11 @@
+package JavaCore.Hash_Java;
+
+import java.util.Scanner;
+
+public class InputHashMap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String
+    }
+}
