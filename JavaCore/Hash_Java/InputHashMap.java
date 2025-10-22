7b7c6e7e572dbf9a4ce4019b2cecd45023008188
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 15:30:01 +0530

    Auto update: 2025-10-22 15:30:01

--- JavaCore/Hash_Java/InputHashMap.java
+++ JavaCore/Hash_Java/InputHashMap.java
@@ -1,16 +1,27 @@
 package JavaCore.Hash_Java;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class InputHashMap {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        HashMap<String, Integer> hashInput = new HashMap<>();
         System.out.println("How many times do you want to enter ?");
         int n = input.nextInt();
         input.nextLine();
         for(int i=0; i<n; i++){
-            
+            System.out.print("Enter your item: ");
+            String key = input.nextLine();
+            System.out.print("Enter your value: ");
+            int value = input.nextInt();
+            input.nextLine();   // Consume the leftover newline
+
+            hashInput.put(key, value);
         }
+        System.out.println("\nItems in the map: ");
+        for(String key : )
 
 
     }
