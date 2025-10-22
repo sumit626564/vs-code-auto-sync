2a4d61dbf4a11ef7fdf3d7957d3136d89f872550
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 18:10:01 +0530

    Auto update: 2025-10-22 18:10:01

--- JavaCore/Array/arrayList.java
+++ JavaCore/Array/arrayList.java
@@ -10,9 +10,28 @@ public class arrayList{
         list.add("apple");
         list.add("Banana");
         list.add("Mango");
+        list.add("Pine-Apple");
+        list.add("Grapes");
 
         // Access element
         System.out.println("First elemnt: "+list.get(0));
-        
+
+        // update element
+        list.set(1, "Orange");
+
+        // Rmove Element
+        list.remove("Mango");   // remove by value
+        list.remove(0);    // remove by index
+
+        // Size of list
+        System.out.println("Size: " + list.size());
+
+        // Loop through ArrayList
+        System.out.println("All elements: ");
+        for(String fruit: list){
+            System.out.print(fruit);
+        }
+
+        System.out.println();
     }
 }
