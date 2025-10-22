247464901473e5915e8031a3706a88535955955e
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-22 13:40:01 +0530

    Auto update: 2025-10-22 13:40:01

--- JavaCore/Hash_Java/HashMAP.java
+++ JavaCore/Hash_Java/HashMAP.java
@@ -1,6 +1,19 @@
 package JavaCore.Hash_Java;
 import java.util.HashMap;
 
+/*
+ *  ===========What is a HashMap..?======================
+ *  A HashMap is a collection in java that stores key-value pairs.
+ *      key--->                 unique (no dulicates)
+ *      Value---->              can be duplicate.
+ *      Order--->               not guaranted (unsroted)
+ *      Nulls---->              allow one {null} key and multiple {null} values
+ *      Performance---->        O(1) for get/put operations on average
+ * 
+ * ================How HashMap Works (Internally)
+ *  Java uses a "hashtable" to store data.
+ *  Each key is hased (using hashCode())--> determines where to store the value.
+ */
 public class HashMAP {
     public static void main(String[] args) {
         
