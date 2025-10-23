2e3e5a76684d96dbd2eeb54a3bff4ef1509a760e
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-23 13:10:01 +0530

    Auto update: 2025-10-23 13:10:01

---
+++ JavaCore/Array/search/SearchCustomObjects.java
@@ -0,0 +1,34 @@
+package JavaCore.Array.search;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Person{
+    String name;
+    int age;
+
+    Person(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+public class SearchCustomObjects {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Person> people = new ArrayList<>();
+
+        people.add(new Person("Sumit",23));
+        people.add(new Person("Rahul", 25));
+        people.add(new Person("Priya", 22));
+
+        System.out.println("Search : Type Any thing: ");
+        String  searchName = input.nextLine();
+        for(Person p : people){      // "p" is a temporarily  Variable
+            if(p.name.equals(searchName)){
+                System.out.println("Found: " + p.name + " age: ");
+            }
+        }
+        
+    }
+}
