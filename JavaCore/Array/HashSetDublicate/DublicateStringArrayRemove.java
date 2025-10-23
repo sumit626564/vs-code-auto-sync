a2b7b9e8ddb4f4befac778f6e2f055518eb7466c
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-23 14:10:01 +0530

    Auto update: 2025-10-23 14:10:01

--- JavaCore/Array/HashSetDublicate/DublicateStringArrayRemove.java
+++ JavaCore/Array/HashSetDublicate/DublicateStringArrayRemove.java
@@ -1,7 +1,16 @@
 package JavaCore.Array.HashSetDublicate;
+import
+import java.util.Arrays;
+import java.util.List;
 
 public class DublicateStringArrayRemove {
     public static void main(String[] args) {
-        String[] words = {"apple","banana","apple","orange","banana","grapes"}
+        String[] words = {"apple","banana","apple","orange","banana","grapes"};
+
+        // Step: 1 Convert array to List
+        List<String> array_list =  Arrays.asList(words);
+
+        // Step 2:- Convert to HashSet (Remove duplicates)
+        Set<String>
     }
 }
