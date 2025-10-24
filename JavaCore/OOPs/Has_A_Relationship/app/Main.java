664fd6b7f5c79e55aa7050bd8190ba4be68c4123
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-24 08:30:01 +0530

    Auto update: 2025-10-24 08:30:01

---
+++ JavaCore/OOPs/Has_A_Relationship/app/Main.java
@@ -0,0 +1,5 @@
+package JavaCore.OOPs.Has_A_Relationship.app;
+
+public class Main {
+    
+}
---
+++ JavaCore/OOPs/Has_A_Relationship/engine/Engine.java
@@ -0,0 +1,11 @@
+package JavaCore.OOPs.Has_A_Relationship.engine;
+
+public class Engine {
+    double power;
+    public Engine(double power){
+        this.power = power;
+    }
+    public double getPower(){
+        return power;
+    }
+}
