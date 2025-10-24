938dc9822d53b5ce0070667b268569aa0d48954b
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-24 08:45:01 +0530

    Auto update: 2025-10-24 08:45:01

--- JavaCore/OOPs/Has_A_Relationship/app/Main.java
+++ JavaCore/OOPs/Has_A_Relationship/app/Main.java
@@ -1,6 +1,12 @@
 package JavaCore.OOPs.Has_A_Relationship.app;
 
+// Import Method
 import JavaCore.OOPs.Has_A_Relationship.car.Car;
+import JavaCore.OOPs.Has_A_Relationship.engine.Engine;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Car c = new Car("audi", 10000, new Engine(269));
+        c.display();
+    }
 }
