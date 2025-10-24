d230ec627e2f4fb9ea11292b7b9941c8ee4aec69
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-24 09:10:01 +0530

    Auto update: 2025-10-24 09:10:01

--- JavaCore/OOPs/Has_A_Relationship/app/Main.java
+++ JavaCore/OOPs/Has_A_Relationship/app/Main.java
@@ -6,6 +6,7 @@ package JavaCore.OOPs.Has_A_Relationship.app;
  *          Syntax:- 
  *                  Option 1: Compile All at Once (Best Way)
  */ 
+
 javac  JavaCore/OOPs/Has_A_Relationship/**/*.java
 
 // Import Method
