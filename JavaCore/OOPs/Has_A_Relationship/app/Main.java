425578906e41abd69e76edc3220bdf3f29b6f1e9
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-24 09:00:01 +0530

    Auto update: 2025-10-24 09:00:01

--- JavaCore/OOPs/Has_A_Relationship/app/Main.java
+++ JavaCore/OOPs/Has_A_Relationship/app/Main.java
@@ -1,7 +1,13 @@
 package JavaCore.OOPs.Has_A_Relationship.app;
 
 
-/* If i have  */
+/* If i have 
+ *      if i have 20, {7 folder 13 file} inside the Has-a-relationship:-
+ *          Syntax:- 
+ *                  Option 1: Compile All at Once (Best Way)
+ *                       //      javac JavaCore/OOPs/Has_A_Relationship/**/*.java //
+
+ */
 // Import Method
 import JavaCore.OOPs.Has_A_Relationship.car.Car;
 import JavaCore.OOPs.Has_A_Relationship.engine.Engine;
