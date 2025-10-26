62ac9ffb246b42a1cd235573e8598eda7582ad13
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 02:30:01 +0530

    Auto update: 2025-10-27 02:30:01

--- JavaCore/MultiThreading/ExtendThread.java
+++ JavaCore/MultiThreading/ExtendThread.java
@@ -1,7 +1,26 @@
 package JavaCore.MultiThreading;
 
 
-class Mythread
+class Mythread1 extends Thread{
+    @Override
+    public void run(){
+        while (true) {
+            System.out.println("My thread is Running");
+            System.out.println("I am Happy");
+        }
+        
+    }
+}
+class Mythread2 extends Thread{
+    @Override
+    public void run(){
+        while (true) {
+            System.out.println("Thread 2 is good..!");
+            System.out.println("I am Sad");
+        }
+        
+    }
+}
 public class ExtendThread {
     public static void main(String[] args) {
         
