c0dfd8249254c693db576dfd28dd14c8629a9bf4
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 03:05:01 +0530

    Auto update: 2025-10-27 03:05:01

--- JavaCore/MultiThreading/RunnableThread.java
+++ JavaCore/MultiThreading/RunnableThread.java
@@ -2,12 +2,19 @@ package JavaCore.MultiThreading;
 
 class MyThreadRunnable1 implements Runnable{
     public void run(){
-        System.out.println("I am a thread 1 not a threat 1");
+        int i = 1;
+        while (i<50000000) {
+            System.out.println("I am a thread 1 not a threat 1");
+
+        }
     }
 }
 class MyThreadRunnable2 implements Runnable{
     public void run(){
-        System.out.println("I am a thread 2 not a threat 2");
+        int i = 1;
+        while (i<10000000) {
+            System.out.println("I am a thread 2 not a threat 2");
+        }
     }
 }
 public class RunnableThread {
