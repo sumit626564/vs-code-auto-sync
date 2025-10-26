8ed31214d48f78fadce5723abb6d51c1eedb1a69
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 03:00:01 +0530

    Auto update: 2025-10-27 03:00:01

--- JavaCore/MultiThreading/RunnableThread.java
+++ JavaCore/MultiThreading/RunnableThread.java
@@ -12,8 +12,15 @@ class MyThreadRunnable2 implements Runnable{
 }
 public class RunnableThread {
     public static void main(String[] args) {
-        MyThreadRunnable1 t1 = new MyThreadRunnable1();
-        MyThreadRunnable2 t2 = new MyThreadRunnable2();
-        t1.start();
+        MyThreadRunnable1 bullet1 = new MyThreadRunnable1();
+        Thread gun1 = new Thread(bullet1);
+
+        MyThreadRunnable2 bullet2 = new MyThreadRunnable2();
+        Thread gun2 = new Thread(bullet2);
+
+        //t1.start();  // we are not abe to run using t1.start
+
+        gun1.start();
+        gun2.start();
     }
 }
