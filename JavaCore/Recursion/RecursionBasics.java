4b53e872cb6a8e08a36480cf5bb2e0897ad4a3e6
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 00:25:01 +0530

    Auto update: 2025-10-28 00:25:01

--- JavaCore/Recursion/RecursionBasics.java
+++ JavaCore/Recursion/RecursionBasics.java
@@ -3,7 +3,7 @@ package JavaCore.Recursion;
 public class RecursionBasics {
     public static void printDec(int n){
         if(n == 1){
-            System.out.println(n);
+            System.out.println(1);
             return;
         }
         System.out.print(n+" ");
