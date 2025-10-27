7c5efd034fce471a637db972e5c259445b7847b2
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 00:50:01 +0530

    Auto update: 2025-10-28 00:50:01

---
+++ JavaCore/Recursion/Factoriral.java
@@ -0,0 +1,15 @@
+package JavaCore.Recursion;
+
+public class Factoriral {
+    public int fact(int n){
+        if(n==0){
+            System.out.println(n);
+            return;
+        }
+        int fnm1 = fact(n-1);
+        int fn = n * f
+    }
+    public static void main(String[] args) {
+        
+    }
+}
