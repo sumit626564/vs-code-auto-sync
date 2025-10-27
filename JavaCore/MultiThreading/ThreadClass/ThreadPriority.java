25a299766518c2dad79c2a6bb25e9d59a7d62109
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 13:10:02 +0530

    Auto update: 2025-10-27 13:10:02

--- JavaCore/MultiThreading/ThreadClass/ThreadPriority.java
+++ JavaCore/MultiThreading/ThreadClass/ThreadPriority.java
@@ -1,6 +1,11 @@
 package JavaCore.MultiThreading.ThreadClass;
 
-/* */
+/* Background Information:
+ *  Java Threads
+ *      The JVM defines a range of ten logical priorities for Java Thread, including:
+ *      sytnax:-
+ *              java.lang.Thread.MIN_pr
+ */
 
 public class ThreadPriority {
 
