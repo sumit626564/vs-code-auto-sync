d2ef379d6a9d14a17a2fc97842de78943fb7edb9
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 14:00:01 +0530

    Auto update: 2025-10-27 14:00:01

--- JavaCore/MultiThreading/ThreadClass/ThreadMethods.java
+++ JavaCore/MultiThreading/ThreadClass/ThreadMethods.java
@@ -1,8 +1,28 @@
 package JavaCore.MultiThreading.ThreadClass;
 
 
+class MyNewThread1 extends Thread{
+    
+    public void run(){
+        while (true) {
+            System.out.println("Thank_you " +  this.getName());
+        } 
+        
+    }
+}
 public class ThreadMethods {
     public static void main(String[] args) {
         
     }
 }
+
+class MyNewThread2 extends Thread{
+    
+    public void run(){
+        while (true) {
+            System.out.println("Thank_you " +  this.getName());
+        } 
+        
+    }
+}
+
