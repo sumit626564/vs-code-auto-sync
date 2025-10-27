6e3b65a09499b1ee96d7b3563aa5326eaea0f7ef
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 12:45:01 +0530

    Auto update: 2025-10-27 12:45:01

---
+++ JavaCore/MultiThreading/ThreadClass/ThreadRunnableStringName.java
@@ -0,0 +1,18 @@
+package JavaCore.MultiThreading.ThreadClass;
+
+
+
+class MyThreadRunnable implements Runnable{
+    public MyThreadRunnable (String name){
+        super(name);
+    }
+    public void run(){
+        int i = 34;
+        System.out.println("The");
+    }
+}
+public class ThreadRunnableStringName {
+    public static void main(String[] args) {
+        
+    }
+}
