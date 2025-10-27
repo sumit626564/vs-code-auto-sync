816cbdde21fc8f98c7427e74319289cf8effccaf
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 01:30:01 +0530

    Auto update: 2025-10-28 01:30:01

--- JavaCore/Recursion/Factoriral.java
+++ JavaCore/Recursion/Factoriral.java
@@ -18,18 +18,21 @@ public class Factoriral {
     }
 
     public static int Fibonacci(int n){
-        if(n==0){
-            return 0;
-        }else if(n==1){
-            return 1;
+        if(n==0 || n == 1){
+            return n;
         }
-        
+        int fbn1 = Fibonacci(n-1); // 1
+        int fbn2 = Fibonacci(n-2); // 1
+        int fbn = fbn1 + fbn2;
+        return fbn;
+
     }
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5000;
         System.out.println("The factorial: "+fact(n));
         System.out.println("=======================");
         System.out.println("The Natural Number: "+ NaturalNumber(n));
         System.out.println("=======================");
+        System.out.println("Fibonacci: "+ Fibonacci(n));
     }
 }
