a96b2f5df90a5bdeb252480593127b3e28a8ba01
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 13:05:01 +0530

    Auto update: 2025-10-27 13:05:01

---
+++ JavaCore/MultiThreading/ThreadClass/ThreadPriority.java
@@ -0,0 +1,11 @@
+package JavaCore.MultiThreading.ThreadClass;
+
+/* */
+
+public class ThreadPriority {
+
+    public static void main(String[] args) {
+        /* Ready Queue: T1, T2, T3, T4, T5 */
+
+    }
+}
