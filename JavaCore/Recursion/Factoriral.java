320a620255e5ce8637a29d7c1f1aa5ad4669e6b7
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 01:20:01 +0530

    Auto update: 2025-10-28 01:20:01

--- JavaCore/Recursion/Factoriral.java
+++ JavaCore/Recursion/Factoriral.java
@@ -18,6 +18,11 @@ public class Factoriral {
     }
 
     public static int Fibonacci(int n){
+        if(n==0){
+            return 0;
+        }else if(n==1){
+            return 1;
+        }
         
     }
     public static void main(String[] args) {
