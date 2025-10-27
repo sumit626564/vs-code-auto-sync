577cafba40e2a949c8c6587e3d5cb7d65b27f556
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 01:10:01 +0530

    Auto update: 2025-10-28 01:10:01

--- JavaCore/Recursion/Factoriral.java
+++ JavaCore/Recursion/Factoriral.java
@@ -9,9 +9,19 @@ public class Factoriral {
         return fn;
     }
 
-    
+    public static int NaturalNumber(int n){
+        if(n==1){
+            return 1;
+        }
+        int natural = n + NaturalNumber(n-1);
+        return natural;
+    }
+
     public static void main(String[] args) {
         int n = 5;
         System.out.println("The factorial: "+fact(n));
+        System.out.println("=======================");
+        System.out.println("The Natural Number: "+ NaturalNumber(n));
+        System.out.println("=======================");
     }
 }
