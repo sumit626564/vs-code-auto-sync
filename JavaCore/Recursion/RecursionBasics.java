99e16b807eccda2fe7778457e3d84f748227b1d8
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 00:40:01 +0530

    Auto update: 2025-10-28 00:40:01

--- JavaCore/Recursion/RecursionBasics.java
+++ JavaCore/Recursion/RecursionBasics.java
@@ -10,6 +10,13 @@ public class RecursionBasics {
         printDec(n+1);
     }
 
+    public static void printInc(int n){
+        if(n==1){
+            
+        }
+        print(n-1);
+        System.out.println(n+" ");
+    }
     public static void main(String[] args) {
         int n = 10;
         printDec(n);
