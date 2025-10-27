f33e98082330b2d94e30479001b09d7addae7050
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-27 14:35:01 +0530

    Auto update: 2025-10-27 14:35:01

---
+++ JavaCore/MultiThreading/Pratice_Set_Thread/Practice_One.java
@@ -0,0 +1,29 @@
+package JavaCore.MultiThreading.Pratice_Set_Thread;
+
+class PraticeThreadOne extends Thread{
+    public void run(){
+        while (true) {
+            System.out.println("Good Morning..!");
+        }
+    }
+}
+class PraticeThreadTwo extends Thread{
+    public void run(){
+        while (true) {
+            Thread.sleep(200); 
+            System.out.println("Welcome");
+        }
+    }
+}
+public class Practice_One {
+    public static void main(String[] args) {
+        PraticeThreadOne p1 = new PraticeThreadOne();
+        PraticeThreadTwo p2 = new PraticeThreadTwo();
+
+        // start thread
+        p1.start();
+        p2.start();
+
+
+    }
+}
