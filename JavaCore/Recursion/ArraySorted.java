23a844a32edd80eac3ce42ae12da28419132f148
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 12:10:01 +0530

    Auto update: 2025-10-28 12:10:01

--- JavaCore/Recursion/ArraySorted.java
+++ JavaCore/Recursion/ArraySorted.java
@@ -2,7 +2,13 @@ package JavaCore.Recursion;
 
 
 class sroted{
-    public is 
+    public static boolean isSorted(int arr[], int i) {
+        if(arr[i] > arr[i+1]){   // array is not sorted terminating point
+            return false;
+        }
+        return isSorted(arr, i+1);
+
+    }
 }
 public class ArraySorted {
     public static void main(String[] args) {
