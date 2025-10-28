63f524af24b704163be2f77b4f7ef5af299d2198
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 12:50:01 +0530

    Auto update: 2025-10-28 12:50:01

--- JavaCore/Recursion/Occurence.java
+++ JavaCore/Recursion/Occurence.java
@@ -4,11 +4,22 @@ package JavaCore.Recursion;
 
 public class Occurence {
     public static int FirstOccurrnce(int[] arr, int key, int index){
+        // if our arr.length == index we will return -1;
+        if(index == arr.length-1){
+            return -1;
+        }
         if(arr[index] == key){
             return index;
         }
+        /* 
+         * if i we don't found the  key on the first index 
+         * then we call FirstOccurrence
+        */
+        return FirstOccurrence(arr, key, index+1);
     }
     public static void main(String[] args) {
-        
+        Occurence occur = new Occurence();
+        int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        System.out.println();
     }    
 }
