011752e12bb2a20c5dd0e134b442dc212b56ea79
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 12:45:01 +0530

    Auto update: 2025-10-28 12:45:01

--- JavaCore/Recursion/Occurence.java
+++ JavaCore/Recursion/Occurence.java
@@ -3,7 +3,11 @@ package JavaCore.Recursion;
 /* WAF to find the first occurence of an element in an array */
 
 public class Occurence {
-    public int FirstOccurrnce(int[] arr, int key, int index)
+    public static int FirstOccurrnce(int[] arr, int key, int index){
+        if(arr[index] == key){
+            return index;
+        }
+    }
     public static void main(String[] args) {
         
     }    
