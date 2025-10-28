cbb7e2a7ee551dd2ea604b200605aa98b90e5e83
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 18:05:01 +0530

    Auto update: 2025-10-28 18:05:01

---
+++ JavaCore/Recursion/Power.java
@@ -0,0 +1,15 @@
+package JavaCore.Recursion;
+
+public class Power {
+    public static int power(int x, int n){
+        if(n==0){
+            return 1;
+        }
+        int xnm1 = power(x, n-1);
+        int xn  = x * xnm1;
+        return xn;
+    }
+    public static void main(String[] args) {
+        System.out.println(power(12, 4));
+    }
+}
