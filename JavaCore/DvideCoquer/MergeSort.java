062a3001dd47b5183e28b9d14be5d8ab19ecdf9d
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 19:05:01 +0530

    Auto update: 2025-10-28 19:05:01

--- JavaCore/DvideCoquer/MergeSort.java
+++ JavaCore/DvideCoquer/MergeSort.java
@@ -4,12 +4,15 @@ public class MergeSort {
 
     public static void printArr(int arr[]){
         for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i]+ " ");
+            System.out.print(arr[i]+ " ");
         }
         System.out.println();
     }
+    public static void mergeSort(int arr[], int starting_index, int ending_index){
+
+    }
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
-        printArr(arr);
+        mergeSort(arr, 0, 0);
     }
 }
