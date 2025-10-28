92ed23105c0019a83af506e4a38faab5c82ddc17
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-28 19:10:01 +0530

    Auto update: 2025-10-28 19:10:01

--- JavaCore/DvideCoquer/MergeSort.java
+++ JavaCore/DvideCoquer/MergeSort.java
@@ -9,10 +9,13 @@ public class MergeSort {
         System.out.println();
     }
     public static void mergeSort(int arr[], int starting_index, int ending_index){
+        // work
+        int mid_index = starting_index +(ending_index - starting_index)/2;  // find out the mid of the array or you can write like this (starting_index + ending_index)/2
+        mergeSort(arr, starting_index, mid_index);
 
     }
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
-        mergeSort(arr, 0, 0);
+        mergeSort(arr, 0, arr.length-1);
     }
 }
