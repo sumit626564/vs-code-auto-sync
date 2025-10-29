08e17f118590fc51b98eaffc76c62163e21fa232
Sumit Singh <rajputsinghshear@gmail.com>
2025-10-29 08:15:01 +0530

    Auto update: 2025-10-29 08:15:01

--- JavaCore/DvideCoquer/quickSort.java
+++ JavaCore/DvideCoquer/quickSort.java
@@ -12,9 +12,13 @@ public class quickSort {
     public static void quickSort(int arr[], int starting_index, int ending_index){
         // last element
         int piviot_index = partition(arr, starting_index, ending_index);
+        quickSort(arr, starting_index, piviot_index-1);  // left index
+        quickSort(arr, piviot_index+1, ending_index); // right index
     }
 
-    p
+    public static int partition(int arr[], int starting_index, int ending_index){
+        
+    }
     public static void main(String[] args) {
         int arr[] = {6, 3, 9, 5, 2, 8};
         quickSort(arr, 0, arr.length-1);
