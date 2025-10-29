7dc3adf5fe3f43f32f9a2c1adb8a2577bf673780
Sumit Singh Rajput <rajputsinghshear@gmail.com>
2025-10-30 04:01:17 +0530

    Auto commit on 30-10-2025  4:01:17.12

--- JavaCore/DvideCoquer/demo.java
+++ JavaCore/DvideCoquer/demo.java
@@ -4,5 +4,7 @@ public class demo {
     public static void main(String[] args) {
         int a = 12;
         System.out.println( a + "13");
+        System.out.println("Arun Pratap Singh");
+        System.out.println("sdhd");
     }
 }
---
+++ JavaCore/DvideCoquer/demonew.java
@@ -0,0 +1,7 @@
+package JavaCore.DvideCoquer;
+
+public class demonew {
+    public static void main(String[] args) {
+        System.out.println("djfhdj");
+    }
+}
