b3356c9afdc456ec6c1e4b38996929edf8a79979
sumit626564 <110198372+sumit626564@users.noreply.github.com>
2025-10-30 03:41:38 +0530

    Auto update 30-10-2025  3:41:38.25

---
+++ JavaCore/DvideCoquer/newdemo.java
@@ -0,0 +1,10 @@
+package JavaCore.DvideCoquer;
+public class newdemo {
+    public static void main(String[] args) {
+        int[] arr = {1, 2,3 ,4 };
+        for(int i= 0; i<arr.length; i++){
+            System.out.print(arr[i]+  ", ");
+        }
+        System.out.println();
+    }
+}
--- autopush.bat
+++ autopush.bat
@@ -1,16 +1,17 @@
 @echo off
-cd /d "D:\Java\vs-code-auto-sync"
+cd /d D:\Java\vs-code-auto-sync
 
-:: Check if there are any changes
-for /f "tokens=*" %%i in ('git status --porcelain') do (
-    set changes=1
-    goto :commit
+REM Check for changes in Git
+git status --porcelain > temp.txt
+
+for /f %%A in (temp.txt) do (
+    git add .
+    git commit -m "Auto update %date% %time%"
+    git push origin main
+    echo [%date% %time%] Auto push successful >> autopush.log
+    del temp.txt
+    exit /b
 )
 
-echo [%date% %time%] - No changes to commit.
-goto :eof
-
-:commit
-git add .
-git commit -m "Auto update: %date% %time%"
-git push origin main
+del temp.txt
+echo [%date% %time%] No changes to commit. >> autopush.log
