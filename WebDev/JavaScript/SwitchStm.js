eb6c3b4630409b4a08659c080861a7aaf60d28e4
Sumit Singh Rajput <rajputsinghshear@gmail.com>
2025-10-31 01:51:13 +0530

    Auto commit on 31-10-2025  1:51:12.94

--- WebDev/JavaScript/SwitchStm.js
+++ WebDev/JavaScript/SwitchStm.js
@@ -1,2 +1,42 @@
 let color = "red";
-switch()
+switch(color){
+    case "red":
+        console.log("slow down");
+        break;
+    case "green":
+        console.log("slow down");
+        break;
+    default:
+        console.log("light is broken");       
+}
+
+/*
+* Use switch statement to print the day of the week usign a number vairable 'day' with values 1 to 7.
+* 1 = Monday, 2 = Tuesday & so on
+*/
+let day = 7;
+switch(day){
+    case(1):
+        console.log("Monday");
+        break;
+    case(2):
+        console.log("Tuesday");
+        break;
+    case(3):
+        console.log("Wednesday");
+        break;
+    case(4):
+        console.log("Thursday");
+        break;
+    case(5):
+        console.log("Friday");
+        break;
+    case(6):
+        console.log("Saturday");
+        break;
+    case(7):
+        console.log("Sunday, fun day");
+        break;
+    default:
+        console.log("wrong day!"); 
+}
---
+++ WebDev/JavaScript/alterPrompt.js
@@ -0,0 +1,7 @@
+let al = alert("danger");
+console.log("This is a simple log" + al);
+console.error("This is an error msg");
+console.warn("this is a warning msg");
+
+let input = prompt("Enter Value: ");
+console.log(input);
