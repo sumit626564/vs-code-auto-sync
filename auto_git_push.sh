261556a1db7b778fc7a75dc7978747a2cc1f84da
Sumit Singh Rajput <rajputsinghshear@gmail.com>
2025-10-31 16:34:01 +0530

    Auto update: 2025-10-31 16:34:01

---
+++ auto_git_push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/cyber-tech/Desktop/vs-code-auto-sync
+
+# Check if there are any changes
+if [ -n "$(git status --porcelain)" ]; then
+    git add .
+    git commit -m "Auto update: $(date '+%Y-%m-%d %H:%M:%S')"
+    git push origin main
+else
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - No changes to commit."
+fi
+
+
+
+rewrite code  for windows
