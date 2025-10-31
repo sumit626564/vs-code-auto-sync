15dc0acff4fc7cc1098da85aec17ed3b1d313e97
Sumit Singh Rajput <rajputsinghshear@gmail.com>
2025-10-31 08:16:12 +0530

    Auto commit on 31-10-2025  8:16:12.88

---
+++ JavaCore/OOPs/TypeCastingOpps/FirstypeCasting.java
@@ -0,0 +1,11 @@
+package JavaCore.OOPs.TypeCastingOpps;
+
+/*
+ * Casting  in Java means converting one type of object or variable into another type.
+ *  In OOPs, casting is mostly about converting between parent and child classes in inheritance hierarchy.
+ */
+public class FirstypeCasting {
+    public static void main(String[] args) {
+        
+    }
+}
