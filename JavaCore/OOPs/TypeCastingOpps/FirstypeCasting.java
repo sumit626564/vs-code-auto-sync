69fbea6b927b03ff1f7d48ada0d997c25e9217c8
Sumit Singh Rajput <rajputsinghshear@gmail.com>
2025-10-31 08:21:13 +0530

    Auto commit on 31-10-2025  8:21:13.00

--- JavaCore/OOPs/TypeCastingOpps/FirstypeCasting.java
+++ JavaCore/OOPs/TypeCastingOpps/FirstypeCasting.java
@@ -3,6 +3,15 @@ package JavaCore.OOPs.TypeCastingOpps;
 /*
  * Casting  in Java means converting one type of object or variable into another type.
  *  In OOPs, casting is mostly about converting between parent and child classes in inheritance hierarchy.
+ * 
+ *  There are ttwo type of casting:
+ *      (1) Primittive Type Casting
+ *          (*) // imlicit (widening)  ---> Automatic conversion (no data loss)
+ *          (*)  // explicit (narrowing)   --> Manual conversion (possible data loss)
+ * 
+ *      (2) Object Type Casting
+ *          (*)   Upcasting (Child --> Parent)
+ *              ""
  */
 public class FirstypeCasting {
     public static void main(String[] args) {
